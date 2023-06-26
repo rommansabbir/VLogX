b25ff71be713bafed8b29fc69924d73d1f011ac4
amiprobashi_amanullahsarker <amanullah.sarker@amiprobashi.com>
2023-06-26 09:59:17 +0600

    add java comment for api

--- vlogx/src/main/java/com/rommansabbir/vlogx/ext/Extensions.kt
+++ vlogx/src/main/java/com/rommansabbir/vlogx/ext/Extensions.kt
@@ -4,12 +4,26 @@ import com.rommansabbir.commander.Command
 import com.rommansabbir.commander.CommanderManager
 import com.rommansabbir.vlogx.core.VLogXService
 
+/**
+ * Send a log to the core service.
+ *
+ * @param log [Any] data to be shown to the log UI.
+ *
+ * @return [Unit]
+ */
 fun sendLog(log: Any) {
     CommanderManager.initialize()
     CommanderManager.getInstance()
         .broadcastCommand(Command(VLogXService.NEW_LOG_COMMAND, log.toString(), VLogXService.UID))
 }
 
+/**
+ * Clear list of logs.
+ *
+ * call this function for clear data is showing in the log UI.
+ *
+ * @return [Unit]
+ */
 fun clearLog() {
     CommanderManager.initialize()
     CommanderManager.getInstance()
@@ -22,6 +36,13 @@ fun clearLog() {
         )
 }
 
+/**
+ * Clear list of logs and close the vlogx view.
+ *
+ * call this function for clear data is showing in the log UI and close he vlogx view.
+ *
+ * @return [Unit]
+ */
 fun clearAndCloseLog() {
     CommanderManager.initialize()
     CommanderManager.getInstance()
